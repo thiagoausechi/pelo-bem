d7d95d7b54a129a801b7574b0680976ca5c2a543
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-06 14:22:40 -0300

    refactor(server/domain): ♻️ atualizar `Owner` para utilizar value object `Phone`

--- backend/domain/entities/owner/owner.ts
+++ backend/domain/entities/owner/owner.ts
@@ -1,15 +1,16 @@
 import { err, ok, type Result } from "@core/result";
 import {
   EmptyPropertyError,
-  InvalidArgumentError,
+  type InvalidArgumentError,
 } from "@server/domain/errors";
 import type { Email } from "@server/domain/value-objects/email";
+import type { Phone } from "@server/domain/value-objects/phone";
 
 interface OwnerProps {
   id?: string;
   fullname: string;
   email: Email;
-  phone: string;
+  phone: Phone;
   profilePicture?: string;
 }
 
@@ -19,7 +20,7 @@ export class Owner {
   public readonly id?: string;
   public readonly fullname: string;
   public readonly email: Email;
-  public readonly phone: string;
+  public readonly phone: Phone;
   public readonly profilePicture?: string;
 
   private constructor(props: OwnerProps) {
@@ -33,16 +34,13 @@ export class Owner {
   public static create(
     props: OwnerProps,
   ): Result<Owner, EmptyPropertyError | InvalidArgumentError> {
-    const { id, fullname, phone, profilePicture } = props;
+    const { id, fullname, profilePicture } = props;
 
     if (id !== undefined && id.length === 0)
       return err(new EmptyPropertyError("ID", Owner.ENTITY_NAME));
 
     if (fullname.length === 0)
       return err(new EmptyPropertyError("Nome Completo", Owner.ENTITY_NAME));
-
-    if (phone.length !== 11)
-      return err(new InvalidArgumentError("Telefone", Owner.ENTITY_NAME));
 
     if (profilePicture !== undefined && profilePicture.length === 0)
       return err(new EmptyPropertyError("Foto de Perfil", Owner.ENTITY_NAME));
