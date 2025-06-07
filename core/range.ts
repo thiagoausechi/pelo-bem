cb15ed69638534ccb2b735d9baf204a48f236a3e
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-06 22:59:59 -0300

    refactor(global): ♻️ atualizar função `inRange` para especificar os tipos de suporte

--- core/range.ts
+++ core/range.ts
@@ -3,7 +3,10 @@ export type Range<T = number> =
   | { min: T; max?: T }
   | { min?: T; max: T };
 
-export function inRange<T>(value: T, range: Range<T>): boolean {
+export function inRange<T extends number | Date | string>(
+  value: T,
+  range: Range<T>,
+): boolean {
   if ("exact" in range) return value === range.exact;
 
   const { min, max } = range;
