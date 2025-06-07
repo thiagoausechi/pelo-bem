d43bc4ccd5e0976f421e258f3cead53fcc105db4
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-06 22:36:57 -0300

    feat(global): ✨ adicionar função utilitária `inRange`

--- core/range.ts
+++ core/range.ts
@@ -2,3 +2,8 @@ export type Range<T = number> = {
   min?: T;
   max?: T;
 };
+
+export function inRange<T>(value: T, range: Range<T>): boolean {
+  const { min, max } = range;
+  return (!min || value >= min) && (!max || value <= max);
+}
