733f9af0e86e747d428b0801fe7091a11f4b52fe
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-07 12:23:20 -0300

    refactor(server/app): ♻️ refatorar parâmetro do método `upload`

--- backend/application/gateways/file-storage.ts
+++ backend/application/gateways/file-storage.ts
@@ -4,11 +4,11 @@ import type { UnexpectedError } from "../errors/unexpected";
 export type FilePath = string;
 
 export interface FileStorageGateway {
-  upload(
-    file: Buffer,
-    path: FilePath,
-    mimeType?: string,
-  ): Promise<Result<FilePath, UnexpectedError>>;
+  upload(args: {
+    file: Buffer;
+    path: FilePath;
+    mimeType?: string;
+  }): Promise<Result<FilePath, UnexpectedError>>;
   download(path: FilePath): Promise<Result<Buffer, UnexpectedError>>;
   delete(path: FilePath): Promise<Result<void, UnexpectedError>>;
   exists(path: FilePath): Promise<Result<boolean, UnexpectedError>>;
