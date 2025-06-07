ee82b76d12c93a92c2b2607a6766fa5c7cce9833
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-07 09:55:42 -0300

    docs(global): 💡 adicionar documentação na função `inRange`

--- core/range.ts
+++ core/range.ts
@@ -3,6 +3,13 @@ export type Range<T = number> =
   | { min: T; max?: T }
   | { min?: T; max: T };
 
+/**
+ * @description Verifica se um valor está dentro de um intervalo.
+ * @template T O tipo do valor e do intervalo. Esta função suporta números, datas e strings. Outros tipos devem ser tratados separadamente.
+ * @param value O valor a ser verificado.
+ * @param range O intervalo a ser verificado.
+ * @returns Verdadeiro se o valor estiver dentro do intervalo, falso caso contrário.
+ */
 export function inRange<T extends number | Date | string>(
   value: T,
   range: Range<T>,
