0c8e990cd0aa5003f1a803f5e4f20f2a9110b032
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-06 22:40:49 -0300

    feat(global): ✨ atualizar tipo `Range` para suportar valores exatos

--- core/range.ts
+++ core/range.ts
@@ -1,9 +1,13 @@
-export type Range<T = number> = {
-  min?: T;
-  max?: T;
-};
+export type Range<T = number> =
+  | { exact: T }
+  | {
+      min?: T;
+      max?: T;
+    };
 
 export function inRange<T>(value: T, range: Range<T>): boolean {
+  if ("exact" in range) return value === range.exact;
+
   const { min, max } = range;
   return (!min || value >= min) && (!max || value <= max);
 }
