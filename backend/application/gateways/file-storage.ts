64d8272408959db640bb46109ead0d415e63ec37
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-07 11:57:01 -0300

    feat(server/app): ✨ adicionar `FileStorageGateway`

---
+++ backend/application/gateways/file-storage.ts
@@ -0,0 +1,15 @@
+import type { Result } from "@core/result";
+import type { UnexpectedError } from "../errors/unexpected";
+
+export type FilePath = string;
+
+export interface FileStorageGateway {
+  upload(
+    file: Buffer,
+    path: FilePath,
+    mimeType?: string,
+  ): Promise<Result<FilePath, UnexpectedError>>;
+  download(path: FilePath): Promise<Result<Buffer, UnexpectedError>>;
+  delete(path: FilePath): Promise<Result<void, UnexpectedError>>;
+  exists(path: FilePath): Promise<Result<boolean, UnexpectedError>>;
+}
