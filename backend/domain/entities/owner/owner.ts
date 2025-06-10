b5fddda0fe8791b8e4dcc4b9d360b9f983829941
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-10 13:22:05 -0300

    refactor(server/domain): ♻️ refatorar `Owner` para extender de `BaseEntity`

--- backend/domain/entities/owner/owner.ts
+++ backend/domain/entities/owner/owner.ts
@@ -5,24 +5,24 @@ import {
 } from "@server/domain/errors";
 import type { Email } from "@server/domain/value-objects/email";
 import type { Phone } from "@server/domain/value-objects/phone";
+import { BaseEntity, type BaseEntityProps } from "../base";
 
-interface OwnerProps {
-  id?: string;
+interface OwnerProps extends BaseEntityProps {
   fullname: string;
   email: Email;
   phone: Phone;
 }
 
-export class Owner {
+export class Owner extends BaseEntity {
   public static readonly ENTITY_NAME = "cuidador";
 
-  public readonly id?: string;
   public readonly fullname: string;
   public readonly email: Email;
   public readonly phone: Phone;
 
   private constructor(props: OwnerProps) {
-    this.id = props.id;
+    super(Owner.ENTITY_NAME, props);
+
     this.fullname = props.fullname;
     this.email = props.email;
     this.phone = props.phone;
@@ -31,14 +31,12 @@ export class Owner {
   public static create(
     props: OwnerProps,
   ): Result<Owner, EmptyPropertyError | InvalidArgumentError> {
-    const { id, fullname } = props;
-
-    if (id !== undefined && id.length === 0)
-      return err(new EmptyPropertyError("ID", Owner.ENTITY_NAME));
+    const { fullname } = props;
 
     if (fullname.length === 0)
       return err(new EmptyPropertyError("Nome Completo", Owner.ENTITY_NAME));
 
-    return ok(new Owner({ ...props }));
+    const owner = new Owner(props);
+    return owner.validate() ?? ok(owner);
   }
 }
