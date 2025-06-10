9531d9db9d1f60a5ccc5bc6e4571402e9286801f
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-10 13:46:13 -0300

    refactor(server/app): ♻️ simplificar o tipo de `filtros` em `Gateway`

--- backend/application/gateways/base/gateway.ts
+++ backend/application/gateways/base/gateway.ts
@@ -1,19 +1,19 @@
-import type { DeepTransformToRange } from "@core/range";
+import type { TransformToRange } from "@core/range";
 import type { Result } from "@core/result";
 import type { NotFoundError } from "@server/application/errors/not-found";
 import type { EntryAlreadyExistsError } from "./entry-already-exists";
 
-export interface BaseGateway<
-  TEntity extends object,
-  TFilters extends
-    DeepTransformToRange<TEntity> = DeepTransformToRange<TEntity>,
-> {
+export type FiltersFor<TEntity extends object> = Partial<
+  TransformToRange<TEntity>
+>;
+
+export interface BaseGateway<TEntity extends object> {
   create(entity: TEntity): Promise<Result<TEntity, EntryAlreadyExistsError>>;
   update(
     entity: Partial<TEntity> & { id: string },
   ): Promise<Result<TEntity, NotFoundError>>;
-  listAll(filters?: TFilters): Promise<TEntity[]>;
-  findBy(filters: TFilters): Promise<Result<TEntity, NotFoundError>>;
-  count(filters?: TFilters): Promise<number>;
-  existsBy(filters: TFilters): Promise<boolean>;
+  listAll(filters?: FiltersFor<TEntity>): Promise<TEntity[]>;
+  findBy(filters: FiltersFor<TEntity>): Promise<Result<TEntity, NotFoundError>>;
+  count(filters?: FiltersFor<TEntity>): Promise<number>;
+  existsBy(filters: FiltersFor<TEntity>): Promise<boolean>;
 }
