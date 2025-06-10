6cfffa388680229216e24ff7ce28237a901600dc
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-10 13:25:56 -0300

    refactor(server/domain): ♻️ refatorar `Veterinarian` para extender de `BaseEntity`

--- backend/domain/entities/veterinarian/veterinarian.ts
+++ backend/domain/entities/veterinarian/veterinarian.ts
@@ -3,26 +3,26 @@ import { EmptyPropertyError } from "@server/domain/errors";
 import type { Email } from "@server/domain/value-objects/email";
 import type { License } from "@server/domain/value-objects/license";
 import type { Phone } from "@server/domain/value-objects/phone";
+import { BaseEntity, type BaseEntityProps } from "../base";
 
-interface VeterinarianProps {
-  id?: string;
+interface VeterinarianProps extends BaseEntityProps {
   fullname: string;
   licenseNumber: License;
   email: Email;
   phone: Phone;
 }
 
-export class Veterinarian {
+export class Veterinarian extends BaseEntity {
   public static readonly ENTITY_NAME = "veterinário";
 
-  public readonly id?: string;
   public readonly fullname: string;
   public readonly licenseNumber: License;
   public readonly email: Email;
   public readonly phone: Phone;
 
   private constructor(props: VeterinarianProps) {
-    this.id = props.id;
+    super(Veterinarian.ENTITY_NAME, props);
+
     this.fullname = props.fullname;
     this.licenseNumber = props.licenseNumber;
     this.email = props.email;
@@ -32,16 +32,14 @@ export class Veterinarian {
   public static create(
     props: VeterinarianProps,
   ): Result<Veterinarian, EmptyPropertyError> {
-    const { id, fullname } = props;
-
-    if (id !== undefined && id.length === 0)
-      return err(new EmptyPropertyError("ID", Veterinarian.ENTITY_NAME));
+    const { fullname } = props;
 
     if (fullname.length === 0)
       return err(
         new EmptyPropertyError("Nome Completo", Veterinarian.ENTITY_NAME),
       );
 
-    return ok(new Veterinarian({ ...props }));
+    const veterinarian = new Veterinarian(props);
+    return veterinarian.validate() ?? ok(veterinarian);
   }
 }
