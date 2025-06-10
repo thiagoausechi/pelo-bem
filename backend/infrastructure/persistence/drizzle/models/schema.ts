38fba9fcf2cfba09903ba35c67d12cb3b44421fd
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-10 00:12:38 -0300

    feat(server/infra): ✨ implementar esquema de banco de dados utilizando `drizzle-orm`

---
+++ backend/infrastructure/persistence/drizzle/models/schema.ts
@@ -0,0 +1,16 @@
+import { env } from "@server/infrastructure/configs/env";
+import { pgSchema, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const appSchema = pgSchema(env.DATABASE_SCHEMA);
+
+export const baseSchema = {
+  id: uuid("id").primaryKey().defaultRandom(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+} as const;
+
+export const schema = {};
