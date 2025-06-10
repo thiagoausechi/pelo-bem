013953e51bb896650f326d9d8a46c638c3207ecb
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-10 12:08:04 -0300

    feat(global): 🏷️ adicionar tipo utilitário para tansformar valores de um objeto em `Range`

--- core/range.ts
+++ core/range.ts
@@ -4,6 +4,43 @@ export type Range<T = number> =
   | { min?: T; max: T };
 
 /**
+ * @description Transforma os valores de um objeto em intervalos.
+ * @example
+ * ```typescript
+ * type Example = {
+ *   name: string;
+ *   age: number;
+ *   birthDate: Date;
+ *   preferences: {
+ *     height: number;
+ *     weight: number;
+ *   };
+ * };
+ *
+ * type TransformedExample = DeepTransformToRange<Example>;
+ * // Result:
+ * // {
+ * //   name: string;
+ * //   age: Range<number>;
+ * //   birthDate: Range<Date>;
+ * //   preferences: {
+ * //     height: Range<number>;
+ * //     weight: Range<number>;
+ * //   };
+ * // }
+ * ```
+ */
+export type DeepTransformToRange<T> = T extends object
+  ? {
+      [K in keyof T]: T[K] extends number
+        ? Range<number>
+        : T[K] extends Date
+          ? Range<Date>
+          : DeepTransformToRange<T[K]>;
+    }
+  : T;
+
+/**
  * @description Verifica se um valor está dentro de um intervalo.
  * @template T O tipo do valor e do intervalo. Esta função suporta números, datas e strings. Outros tipos devem ser tratados separadamente.
  * @param value O valor a ser verificado.
