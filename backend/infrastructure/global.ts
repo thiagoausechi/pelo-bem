c7ab53338387ec6d699ff319863a3014aca9d17e
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-11 09:22:32 -0300

    feat(server/infra): ✨ adicionar `GlobalGateways`

--- backend/infrastructure/global.ts
+++ backend/infrastructure/global.ts
@@ -1,6 +1,11 @@
+import { PgOwnerGateway } from "./persistence/drizzle/gateways/owner";
 import { ZodEmailValidator, ZodPhoneValidator } from "./validators/zod";
 
 export const GlobalValidators = {
   emailValidator: new ZodEmailValidator(),
   phoneValidator: new ZodPhoneValidator(),
+};
+
+export const GlobalGateways = {
+  ownerGateway: new PgOwnerGateway(GlobalValidators),
 };
