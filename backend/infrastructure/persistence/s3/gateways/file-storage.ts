8faa3ef8e508547904054ce1d3bcf3cb0a699d78
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-11 12:15:43 -0300

    feat(server/infra): ✨ implementar `upload` de `S3FileStorageGateway`

--- backend/infrastructure/persistence/s3/gateways/file-storage.ts
+++ backend/infrastructure/persistence/s3/gateways/file-storage.ts
@@ -1,17 +1,39 @@
-import type { Result } from "@core/result";
-import type { UnexpectedError } from "@server/application/errors/unexpected";
+import { PutObjectCommand, type S3Client } from "@aws-sdk/client-s3";
+import { err, ok, type Result } from "@core/result";
+import { UnexpectedError } from "@server/application/errors/unexpected";
 import type {
   FilePath,
   FileStorageGateway,
 } from "@server/application/gateways/file-storage";
 
 export class S3FileStorageGateway implements FileStorageGateway {
-  upload(args: {
+  constructor(
+    private readonly s3Client: S3Client,
+    private readonly bucketName: string,
+  ) {}
+
+  async upload(args: {
     file: Buffer;
     path: FilePath;
     mimeType?: string;
   }): Promise<Result<FilePath, UnexpectedError>> {
-    throw new Error("Method not implemented.");
+    const command = new PutObjectCommand({
+      Bucket: this.bucketName,
+      Key: args.path,
+      Body: args.file,
+      ContentType: args.mimeType,
+      ACL: "public-read",
+    });
+
+    try {
+      await this.s3Client.send(command);
+      return ok(args.path);
+    } catch (error) {
+      console.error("Erro ao fazer upload do arquivo para o S3:", error);
+      return err(
+        new UnexpectedError("Erro ao fazer upload do arquivo", error as Error),
+      );
+    }
   }
 
   download(path: FilePath): Promise<Result<Buffer, UnexpectedError>> {
