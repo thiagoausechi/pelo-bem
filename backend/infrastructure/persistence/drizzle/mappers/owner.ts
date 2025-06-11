28f36d9690fadc2254370d1281100c0f17a0f770
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-11 07:25:48 -0300

    feat(server/infra): ✨ implementar `PgOwnerMapper` para conversão entre entity e model

---
+++ backend/infrastructure/persistence/drizzle/mappers/owner.ts
@@ -0,0 +1,51 @@
+import { Owner } from "@server/domain/entities/owner";
+import { Email, type EmailValidator } from "@server/domain/value-objects/email";
+import { Phone, type PhoneValidator } from "@server/domain/value-objects/phone";
+import type { PgOwnerInsertModel, PgOwnerModel } from "../models/owner";
+
+interface Dependencies {
+  emailValidator: EmailValidator;
+  phoneValidator: PhoneValidator;
+}
+
+export class PgOwnerMapper {
+  constructor(private readonly deps: Dependencies) {}
+
+  async toModel(entity: Owner): Promise<PgOwnerInsertModel> {
+    return {
+      id: entity.id,
+      fullname: entity.fullname,
+      email: entity.email.get(),
+      phone: entity.phone.get(),
+      createdAt: entity.createdAt,
+      updatedAt: entity.updatedAt,
+    };
+  }
+
+  async toEntity(model: PgOwnerModel): Promise<Owner> {
+    const email = Email.create({
+      email: model.email,
+      emailValidator: this.deps.emailValidator,
+    });
+    if (!email.ok) throw email.error;
+
+    const phone = Phone.create({
+      phone: model.phone,
+      phoneValidator: this.deps.phoneValidator,
+    });
+    if (!phone.ok) throw phone.error;
+
+    const owner = Owner.create({
+      id: model.id,
+      fullname: model.fullname,
+      email: email.value,
+      phone: phone.value,
+      createdAt: model.createdAt,
+      updatedAt: model.updatedAt,
+    });
+
+    if (!owner.ok) throw owner.error;
+
+    return owner.value;
+  }
+}
