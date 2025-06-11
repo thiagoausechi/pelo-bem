d32a777d59b7353e51db8333516636031b0b3705
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-11 07:33:52 -0300

    fix(server/infra): 🐛 corrigir a inicialização do `appSchema` para lidar com o schema "public"

--- backend/infrastructure/persistence/drizzle/models/base.ts
+++ backend/infrastructure/persistence/drizzle/models/base.ts
@@ -1,7 +1,10 @@
 import { env } from "@server/infrastructure/configs/env";
 import { pgSchema, timestamp, uuid } from "drizzle-orm/pg-core";
 
-export const appSchema = pgSchema(env.DATABASE_SCHEMA);
+export const appSchema = env.DATABASE_SCHEMA
+  ? pgSchema(env.DATABASE_SCHEMA)
+  : // @ts-expect-error: `pgSchema` requer 1 argumento, mas o Drizzle pede nenhum caso o schema seja "public"
+    pgSchema();
 
 export const baseSchema = {
   id: uuid("id").primaryKey().defaultRandom(),
