fe2dfdace4b83a292269c0243a855c68a5ec9e24
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-13 01:32:29 -0300

    feat(client/pages): ✨ adicionar página `registers`

---
+++ app/(app)/registers/page.tsx
@@ -0,0 +1,5 @@
+import { RegistersPage } from "@client/pages/registers";
+
+export default function Registers() {
+  return <RegistersPage />;
+}
---
+++ frontend/pages/registers/index.tsx
@@ -0,0 +1,11 @@
+export function RegistersPage() {
+  return (
+    <div className="relative isolate flex flex-1 flex-col">
+      <div className="@container/content container mx-auto flex flex-1 flex-col gap-2 px-4">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          {/* tabs */}
+        </div>
+      </div>
+    </div>
+  );
+}
