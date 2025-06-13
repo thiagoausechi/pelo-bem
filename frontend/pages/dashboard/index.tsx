79b3bef6815524096bf6d69a0b8fbb69a344bf78
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-12 23:57:20 -0300

    feat(client/components): ✨ adicionar componente `StatsCardSection`

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -1,9 +1,11 @@
+import { StatsCardSection } from "./sections";
+
 export function DashboardPage() {
   return (
     <div className="relative isolate flex flex-1 flex-col">
       <div className="@container/content container mx-auto flex flex-1 flex-col gap-2 px-4">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          {/* <StatsCardSection /> */}
+          <StatsCardSection />
 
           {/* <UpcomingServicesSection /> */}
 
---
+++ frontend/pages/dashboard/sections/stats-card/index.tsx
@@ -0,0 +1,9 @@
+export function StatsCardSection() {
+  return (
+    <section className="grid grid-cols-1 gap-4 @xl/content:grid-cols-2 @5xl/content:grid-cols-4">
+      {/* <RevenueCard /> */}
+      {/* <AppointmentsTodayCard /> */}
+      {/* <RatingsCard /> */}
+    </section>
+  );
+}
