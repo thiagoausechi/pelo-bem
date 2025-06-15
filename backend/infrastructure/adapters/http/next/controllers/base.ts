61eea5a03473c652dc36dcd2cdc902d6552ae19f
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-15 13:30:39 -0300

    refactor(server/infra): ♻️ remover lógica de tratamento de erros do middleware

---
+++ backend/infrastructure/adapters/http/next/controllers/base.ts
@@ -0,0 +1,38 @@
+import { HttpStatus } from "@core/http";
+import { ApplicationError } from "@server/application/errors";
+import { NotFoundError } from "@server/application/errors/not-found";
+import { UnexpectedError } from "@server/application/errors/unexpected";
+import { EntryAlreadyExistsError } from "@server/application/gateways/base/entry-already-exists";
+import { DomainError } from "@server/domain/errors";
+import type { NextResponse } from "next/server";
+
+export class NextJsController {
+  protected async handleRequest(handler: () => Promise<NextResponse>) {
+    try {
+      const response = await handler();
+      return response;
+    } catch (error) {
+      console.error("[API]", error);
+
+      if (!(error instanceof Error))
+        return HttpStatus.INTERNAL_SERVER_ERROR(
+          "Ocorreu um erro inesperado.",
+        )();
+
+      if (error instanceof DomainError)
+        return HttpStatus.BAD_REQUEST(error.message)();
+
+      if (error instanceof ApplicationError) {
+        if (error instanceof EntryAlreadyExistsError)
+          return HttpStatus.CONFLICT(error)();
+
+        if (error instanceof NotFoundError)
+          return HttpStatus.NOT_FOUND(error)();
+      }
+      if (error instanceof UnexpectedError)
+        return HttpStatus.INTERNAL_SERVER_ERROR(error)();
+
+      return HttpStatus.UNHANDLED_ERROR(error)();
+    }
+  }
+}
--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,30 @@
-import { HttpStatus } from "@core/http";
-import { ApplicationError } from "@server/application/errors";
-import { NotFoundError } from "@server/application/errors/not-found";
-import { UnexpectedError } from "@server/application/errors/unexpected";
-import { EntryAlreadyExistsError } from "@server/application/gateways/base/entry-already-exists";
-import { DomainError } from "@server/domain/errors";
 import { type NextRequest, NextResponse } from "next/server";
 
 export function middleware(_: NextRequest): NextResponse {
-  try {
-    return NextResponse.next();
-  } catch (error) {
-    console.error("[API]", error);
+  // try {
+  //   console.log("[API] Middleware executed");
+  //   return NextResponse.next();
+  // } catch (error) {
+  //   console.error("[API]", error);
 
-    if (!(error instanceof Error))
-      return HttpStatus.INTERNAL_SERVER_ERROR("Ocorreu um erro inesperado.")();
+  //   if (!(error instanceof Error))
+  //     return HttpStatus.INTERNAL_SERVER_ERROR("Ocorreu um erro inesperado.")();
 
-    if (error instanceof DomainError)
-      return HttpStatus.BAD_REQUEST(error.message)();
+  //   if (error instanceof DomainError)
+  //     return HttpStatus.BAD_REQUEST(error.message)();
 
-    if (error instanceof ApplicationError) {
-      if (error instanceof EntryAlreadyExistsError)
-        return HttpStatus.CONFLICT(error)();
+  //   if (error instanceof ApplicationError) {
+  //     if (error instanceof EntryAlreadyExistsError)
+  //       return HttpStatus.CONFLICT(error)();
 
-      if (error instanceof NotFoundError) return HttpStatus.NOT_FOUND(error)();
-    }
-    if (error instanceof UnexpectedError)
-      return HttpStatus.INTERNAL_SERVER_ERROR(error)();
+  //     if (error instanceof NotFoundError) return HttpStatus.NOT_FOUND(error)();
+  //   }
+  //   if (error instanceof UnexpectedError)
+  //     return HttpStatus.INTERNAL_SERVER_ERROR(error)();
 
-    return HttpStatus.UNHANDLED_ERROR(error)();
-  }
+  //   return HttpStatus.UNHANDLED_ERROR(error)();
+  // }
+  return NextResponse.next();
 }
 
 export const config = {
