cf70a9bf36639fd1d181b49c01e0ec3ddb304dc1
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-15 13:37:55 -0300

    refactor(server/infra): ♻️ alterar lógica de instâncias globais

--- backend/infrastructure/factories.ts
+++ backend/infrastructure/factories.ts
@@ -1,15 +1,31 @@
 import { env } from "./configs/env";
-import { PgOwnerGateway } from "./persistence/drizzle/gateways/owner";
+import { PgOwnerGateway } from "./persistence/drizzle/gateways";
 import { s3 } from "./persistence/s3";
 import { S3FileStorageGateway } from "./persistence/s3/gateways";
 import { ZodEmailValidator, ZodPhoneValidator } from "./validators/zod";
 
-export const GlobalValidators = {
-  emailValidator: new ZodEmailValidator(),
-  phoneValidator: new ZodPhoneValidator(),
+const globalForDependencies = globalThis as Record<string, unknown>;
+
+function getOrCreate<T>(key: string, creator: () => T): T {
+  const instance = globalForDependencies[key] ?? creator();
+
+  if (env.NODE_ENV !== "production") globalForDependencies[key] = instance;
+
+  return instance as T;
+}
+
+export const Validators = {
+  emailValidator: getOrCreate("emailValidator", () => new ZodEmailValidator()),
+  phoneValidator: getOrCreate("phoneValidator", () => new ZodPhoneValidator()),
 };
 
-export const GlobalGateways = {
-  ownerGateway: new PgOwnerGateway(GlobalValidators),
-  fileStorageGateway: new S3FileStorageGateway(s3, env.S3_BUCKET_NAME),
+export const Gateways = {
+  ownerGateway: getOrCreate(
+    "ownerGateway",
+    () => new PgOwnerGateway(Validators),
+  ),
+  fileStorage: getOrCreate(
+    "fileStorage",
+    () => new S3FileStorageGateway(s3, env.S3_BUCKET_NAME),
+  ),
 };
