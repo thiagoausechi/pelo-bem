5d8416686673d78d5d4b517e7d06f0f121d8e5db
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-15 14:02:51 -0300

    refactor(server/infra): ♻️ refatorar `NextJsOwnersController` para incluir dependências tipadas

--- backend/infrastructure/adapters/http/next/controllers/owners.ts
+++ backend/infrastructure/adapters/http/next/controllers/owners.ts
@@ -1,7 +1,20 @@
 import { HttpStatus } from "@core/http";
+import type { OwnerGateway } from "@server/application/gateways";
+import type { FileStorageGateway } from "@server/application/gateways/file-storage";
+import type { EmailValidator } from "@server/domain/value-objects/email";
+import type { PhoneValidator } from "@server/domain/value-objects/phone";
 import { type NextRequest, type NextResponse } from "next/server";
 
+interface Dependencies {
+  ownerGateway: OwnerGateway;
+  emailValidator: EmailValidator;
+  phoneValidator: PhoneValidator;
+  fileStorage: FileStorageGateway;
+}
+
 export class NextJsOwnersController {
+  constructor(private readonly deps: Dependencies) {}
+
   async handleGet(_: NextRequest): Promise<NextResponse> {
     return HttpStatus.NOT_IMPLEMENTED();
   }
