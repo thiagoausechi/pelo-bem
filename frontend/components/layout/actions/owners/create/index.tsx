0b9f73d6f66b64b602e9911f2abfae32583e0bd5
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-16 16:06:51 -0300

    refactor(client/components): ♻️ atualizar `CreateOwnerAction` para aceitar gatilho (trigger) customizado

--- frontend/components/layout/actions/owners/create/index.tsx
+++ frontend/components/layout/actions/owners/create/index.tsx
@@ -6,14 +6,15 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@client/components/ui/drawer";
+import type { PropsWithChildren } from "react";
 import { CreateOwnerForm } from "./form";
 
-export function CreateOwnerAction() {
+export function CreateOwnerAction({
+  children = <Button size="sm">Novo Cuidador</Button>,
+}: PropsWithChildren) {
   return (
     <Drawer>
-      <DrawerTrigger asChild>
-        <Button size="sm">Novo Cuidador</Button>
-      </DrawerTrigger>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
       <DrawerContent>
         <DrawerHeader className="gap-1">
           <DrawerTitle>Adicionar novo Cuidador</DrawerTitle>
