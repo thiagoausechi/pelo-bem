28efe1fc1b5804c466714d542282efa4b7c2faa1
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-16 13:26:53 -0300

    feat(client): ✨ adicionar componente `Toaster` ao `RootLayout`

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { type Metadata } from "next";
 import type { PropsWithChildren } from "react";
 
 import { ClientProviders } from "@client/components/providers";
+import { Toaster } from "@client/components/ui/sonner";
 import { lora } from "@client/styles/fonts";
 
 export const metadata: Metadata = {
@@ -15,6 +16,12 @@ export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
     <html lang="pt-BR" className={`${lora.variable}`}>
       <body>
+        <Toaster
+          position="bottom-center"
+          theme="light"
+          closeButton
+          richColors
+        />
         <ClientProviders>{children}</ClientProviders>
       </body>
     </html>
