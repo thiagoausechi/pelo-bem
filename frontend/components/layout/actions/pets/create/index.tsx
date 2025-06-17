66388903d91fd7ca70387045ef56b62a6169c310
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-17 20:13:41 -0300

    fix(client/components): 🚨 remover hidratação em componente `client-side`

--- frontend/components/layout/actions/pets/create/index.tsx
+++ frontend/components/layout/actions/pets/create/index.tsx
@@ -6,39 +6,21 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@client/components/ui/drawer";
-import {
-  fetchOwnersDropdownList,
-  ownersDropdownListQueryKey,
-} from "@client/services/owners";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
 import type { PropsWithChildren } from "react";
 import { CreatePetForm } from "./form";
 
-export async function CreatePetAction({
+export function CreatePetAction({
   children = <Button size="sm">Novo Pet</Button>,
 }: PropsWithChildren) {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ownersDropdownListQueryKey,
-    queryFn: fetchOwnersDropdownList,
-  });
-
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Drawer>
-        <DrawerTrigger asChild>{children}</DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader className="gap-1">
-            <DrawerTitle>Adicionar novo Pet</DrawerTitle>
-          </DrawerHeader>
-          <CreatePetForm />
-        </DrawerContent>
-      </Drawer>
-    </HydrationBoundary>
+    <Drawer>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="gap-1">
+          <DrawerTitle>Adicionar novo Pet</DrawerTitle>
+        </DrawerHeader>
+        <CreatePetForm />
+      </DrawerContent>
+    </Drawer>
   );
 }
