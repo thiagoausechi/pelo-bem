bf4c26383eecd6af1fa826c1e12a280d7202b049
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-17 11:53:59 -0300

    refactor(server/app): ♻️ atualizar resposta do `ListPetsUseCase`

--- backend/application/usecases/pet/list.ts
+++ backend/application/usecases/pet/list.ts
@@ -4,6 +4,7 @@ import type {
   FiltersFor,
   ListOptions,
 } from "@server/application/gateways/base/gateway";
+import type { Owner } from "@server/domain/entities/owner";
 import type { Pet } from "@server/domain/entities/pet";
 
 interface Dependencies {
@@ -17,18 +18,33 @@ interface Request {
 }
 
 interface Response {
-  pets: Entry<Pet>[];
+  pet: Entry<Pet>;
+  owner: Entry<Owner>;
 }
 
 export class ListPetsUseCase {
   constructor(private deps: Dependencies) {}
 
-  async execute(request: Request): Promise<Response> {
+  async execute(request: Request): Promise<Response[]> {
     const pets = await this.deps.petGateway.listAll(
       request.filters,
       request.options,
     );
 
-    return { pets };
+    const owners = await Promise.all(
+      pets.map((pet) => this.deps.ownerGateway.findBy({ id: pet.ownerId })),
+    );
+
+    const ownersValues = owners.map((owner) => {
+      // Teóricamente este throw nunca irá acontecer
+      // mas é necessário para garantir o `.value` no retorno
+      if (!owner.ok) throw owner.error;
+      return owner.value;
+    });
+
+    return pets.map((pet, index) => ({
+      pet,
+      owner: ownersValues[index]!,
+    }));
   }
 }
