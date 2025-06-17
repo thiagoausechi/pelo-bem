153b742538089919e2247bd70a840514dfe39ed2
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-17 11:02:46 -0300

    feat(global): ✨ adicionar `createPetForm`

---
+++ core/contracts/forms/pet.ts
@@ -0,0 +1,17 @@
+import { genders, species } from "@server/domain/entities/pet";
+import { z } from "zod";
+import { avatarPictureSchema } from "../avatar-picutre";
+
+export const createPetForm = z.object({
+  ownerId: z.string().min(1, "ID do dono é obrigatório."),
+  name: z.string().min(1, "Nome é obrigatório."),
+  specie: z.enum(species),
+  breed: z.string().min(1, "Raça é obrigatória."),
+  birthday: z.coerce.date(),
+  weightKg: z.coerce.number().min(0, "Peso deve ser um número positivo."),
+  heightCm: z.coerce.number().min(0, "Altura deve ser um número positivo."),
+  gender: z.enum(genders),
+  picture: avatarPictureSchema.optional(),
+});
+
+export type CreatePetFormData = z.infer<typeof createPetForm>;
