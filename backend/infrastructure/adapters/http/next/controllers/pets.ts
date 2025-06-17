d65d2d5636db7db8b9d0076ee1467fcc493e1776
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-17 11:24:13 -0300

    feat(server/infra): ✨ implementar `handlePost` em `NextJsPetsController`

--- backend/infrastructure/adapters/http/next/controllers/pets.ts
+++ backend/infrastructure/adapters/http/next/controllers/pets.ts
@@ -1,6 +1,8 @@
+import { createPetForm } from "@core/contracts/forms/pet";
 import { HttpStatus } from "@core/http";
 import type { OwnerGateway, PetGateway } from "@server/application/gateways";
 import type { FileStorageGateway } from "@server/application/gateways/file-storage";
+import { CreatePetUseCase } from "@server/application/usecases/pet";
 import type { NextRequest, NextResponse } from "next/server";
 import { NextJsController } from "./base";
 
@@ -19,8 +21,23 @@ export class NextJsPetsController extends NextJsController {
     return HttpStatus.NOT_IMPLEMENTED();
   }
 
-  async handlePost(_: NextRequest): Promise<NextResponse> {
-    return HttpStatus.NOT_IMPLEMENTED();
+  async handlePost(request: NextRequest): Promise<NextResponse> {
+    if (this.parsePath(request).length > 0)
+      return HttpStatus.BAD_REQUEST(
+        "Caminho de requisição inválido para o método POST.",
+      );
+
+    return this.handleRequest(async () => {
+      const parsedFormData = await this.parseRequest(createPetForm, request);
+
+      const useCaseResponse = await new CreatePetUseCase(this.deps).execute(
+        parsedFormData,
+      );
+
+      if (!useCaseResponse.ok) throw useCaseResponse.error;
+
+      return HttpStatus.CREATED(useCaseResponse.value);
+    });
   }
 
   async handlePut(_: NextRequest): Promise<NextResponse> {
