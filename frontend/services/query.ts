7265b7991a7a2fdb360eecceef6ccd1303e959f3
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-17 13:15:29 -0300

    feat(client/services): ✨ adicionar função utilitária `makeMutation`

--- frontend/services/query.ts
+++ frontend/services/query.ts
@@ -14,3 +14,23 @@ export function makeQuery<TResponseData>(
     return body.data;
   };
 }
+
+export function makeMutation<TRequestData, TResponseData>(
+  url: string,
+  options: {
+    valuesToFormData: (values: TRequestData) => FormData;
+  },
+): (values: TRequestData) => Promise<TResponseData> {
+  return async (values: TRequestData) => {
+    const response = await fetch(`/api${url}`, {
+      method: "POST",
+      body: options.valuesToFormData(values),
+    });
+
+    const body = (await response.json()) as HttpResponse<TResponseData>;
+
+    if ("error" in body) throw body.error;
+
+    return body.data;
+  };
+}
