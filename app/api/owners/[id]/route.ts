1781138c48e2a4e37518294aed915dad25a3c374
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-17 04:22:49 -0300

    feat(server/infra): ✨ adicionar rota para CRUD de usuário único

---
+++ app/api/owners/[id]/route.ts
@@ -0,0 +1,16 @@
+import { Controllers } from "@server/infrastructure/factories";
+import type { NextRequest, NextResponse } from "next/server";
+
+const controller = Controllers.ownersController;
+
+export async function GET(request: NextRequest): Promise<NextResponse> {
+  return controller.handleGet(request);
+}
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  return controller.handlePost(request);
+}
+
+export async function PUT(request: NextRequest): Promise<NextResponse> {
+  return controller.handlePut(request);
+}
