1cfd02fc3b1d963270e0778fbfa62f2dc496e97c
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-16 23:41:24 -0300

    feat(server/infra): ✨ adicionar implementação `PgPetGateway`

---
+++ backend/infrastructure/persistence/drizzle/gateways/pet.ts
@@ -0,0 +1,45 @@
+import type { Result } from "@core/result";
+import type { NotFoundError } from "@server/application/errors/not-found";
+import type { PetGateway } from "@server/application/gateways";
+import type { EntryAlreadyExistsError } from "@server/application/gateways/base/entry-already-exists";
+import type {
+  Entry,
+  FiltersFor,
+  ListOptions,
+} from "@server/application/gateways/base/gateway";
+import type { Pet } from "@server/domain/entities/pet";
+
+export class PgPetGateway implements PetGateway {
+  async create(
+    entity: Pet,
+  ): Promise<Result<Entry<Pet>, EntryAlreadyExistsError>> {
+    throw new Error("Method not implemented.");
+  }
+
+  async update(
+    entity: Partial<Pet> & { id: string },
+  ): Promise<Result<Entry<Pet>, NotFoundError>> {
+    throw new Error("Method not implemented.");
+  }
+
+  async listAll(
+    filters?: FiltersFor<Pet>,
+    options?: ListOptions<keyof Pet>,
+  ): Promise<Entry<Pet>[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findBy(
+    filters: FiltersFor<Pet>,
+  ): Promise<Result<Entry<Pet>, NotFoundError>> {
+    throw new Error("Method not implemented.");
+  }
+
+  async count(filters?: FiltersFor<Pet>): Promise<number> {
+    throw new Error("Method not implemented.");
+  }
+
+  async existsBy(filters: FiltersFor<Pet>): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+}
