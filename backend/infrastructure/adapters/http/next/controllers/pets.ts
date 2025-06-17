a34da30d76b4f0680176b5433bc233d0738923b2
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-17 12:28:33 -0300

    feat(server/infra): ✨ implementar `handleGet` em `NextJsPetsController`

--- backend/infrastructure/adapters/http/next/controllers/pets.ts
+++ backend/infrastructure/adapters/http/next/controllers/pets.ts
@@ -1,8 +1,12 @@
+import { calculateAge } from "@core/age";
+import type { PetDTO } from "@core/contracts/dtos/pets";
 import { createPetForm } from "@core/contracts/forms/pet";
 import { HttpStatus } from "@core/http";
 import type { OwnerGateway, PetGateway } from "@server/application/gateways";
 import type { FileStorageGateway } from "@server/application/gateways/file-storage";
 import { CreatePetUseCase } from "@server/application/usecases/pet";
+import { ListPetsUseCase } from "@server/application/usecases/pet/list";
+import { env } from "@server/infrastructure/configs/env";
 import type { NextRequest, NextResponse } from "next/server";
 import { NextJsController } from "./base";
 
@@ -17,8 +21,33 @@ export class NextJsPetsController extends NextJsController {
     super();
   }
 
-  async handleGet(_: NextRequest): Promise<NextResponse> {
-    return HttpStatus.NOT_IMPLEMENTED();
+  async handleGet(request: NextRequest): Promise<NextResponse> {
+    return this.handleRequest(async () => {
+      const pathSegments = this.parsePath(request);
+      const id = pathSegments.length > 0 ? pathSegments[0] : undefined;
+
+      const useCaseResponse = await new ListPetsUseCase(this.deps).execute({
+        filters: id ? { id } : undefined,
+      });
+
+      // TODO: Criar um mapper para os DTOs
+      const result: PetDTO[] = useCaseResponse.map(({ pet, owner }) => ({
+        ...pet,
+        picture: `${env.S3_PUBLIC_URL}/pets/${pet.id}.png`,
+        age: calculateAge(pet.birthday),
+
+        ownerId: undefined,
+        owner: {
+          ...owner,
+          profile: `${env.S3_PUBLIC_URL}/owners/${owner.id}.png`,
+          name: owner.fullname,
+          email: owner.email.get(),
+          phone: owner.phone.get(),
+        },
+      }));
+
+      return HttpStatus.OK(result);
+    });
   }
 
   async handlePost(request: NextRequest): Promise<NextResponse> {
