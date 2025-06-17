d4c33cb32c6bb4508eb8e1b14787bad34d73a96e
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-17 17:13:59 -0300

    fix(server/domain): 💬 ajustar valores de peso e altura para espécies

--- backend/domain/entities/pet/species.enum.ts
+++ backend/domain/entities/pet/species.enum.ts
@@ -11,17 +11,17 @@ export interface SpecieInfo {
 export const SpeciesInfo: Record<Specie, SpecieInfo> = {
   DOG: {
     label: "cachorro",
-    weightKg: { min: 0.2, max: 100 },
+    weightKg: { min: 0.5, max: 100 },
     heightCm: { min: 10, max: 110 },
   },
   CAT: {
     label: "gato",
-    weightKg: { min: 0.05, max: 12 },
-    heightCm: { min: 5, max: 30 },
+    weightKg: { min: 0.3, max: 12 },
+    heightCm: { min: 10, max: 35 },
   },
   BIRD: {
     label: "pássaro",
-    weightKg: { min: 2, max: 1.5 },
-    heightCm: { min: 3, max: 100 },
+    weightKg: { min: 0.015, max: 15 },
+    heightCm: { min: 5, max: 120 },
   },
 } as const;
