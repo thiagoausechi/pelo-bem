7e1cadc05468019f97112e76cdf6ee75a07cb851
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-17 22:45:45 -0300

    feat(global): ✨ adicionar `createVeterinatianForm`

---
+++ core/contracts/forms/veterinarian.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+import { avatarPictureSchema } from "../avatar-picutre";
+
+export const createVeterinatianForm = z.object({
+  fullname: z.string().min(1, "Nome é obrigatório."),
+  email: z.string().min(1, "E-mail é obrigatório."),
+  phone: z.string().min(1, "Telefone é obrigatório."),
+  licenseNumber: z.string().min(1, "Número da licença é obrigatório."),
+  profilePicture: avatarPictureSchema.optional(),
+});
+
+export type CreateVeterinarianFormData = z.infer<typeof createVeterinatianForm>;
