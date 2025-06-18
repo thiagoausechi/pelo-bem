8926b6bc8c43d1c7fac962cccae6858c5609546a
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-17 23:18:50 -0300

    feat(server/infra): ✨ implementar `PgVeterinarianMapper` para conversão entre entity e model

---
+++ backend/infrastructure/persistence/drizzle/mappers/veterinarian.ts
@@ -0,0 +1,81 @@
+import { Veterinarian } from "@server/domain/entities/veterinarian";
+import { Email, type EmailValidator } from "@server/domain/value-objects/email";
+import {
+  License,
+  type LicenseValidator,
+} from "@server/domain/value-objects/license";
+import { Phone, type PhoneValidator } from "@server/domain/value-objects/phone";
+import type {
+  PgVeterinarianInsertModel,
+  PgVeterinarianModel,
+} from "../models/veterinarians";
+
+interface Dependencies {
+  emailValidator: EmailValidator;
+  phoneValidator: PhoneValidator;
+  licenseValidator: LicenseValidator;
+}
+
+export class PgVeterinarianMapper {
+  constructor(private readonly deps: Dependencies) {}
+
+  async toModel(entity: Veterinarian): Promise<PgVeterinarianInsertModel> {
+    return {
+      id: entity.id,
+      fullname: entity.fullname,
+      email: entity.email.get(),
+      phone: entity.phone.get(),
+      license: entity.licenseNumber.get(),
+      createdAt: entity.createdAt,
+      updatedAt: entity.updatedAt,
+    };
+  }
+
+  async toPartialModel(
+    entity: Partial<Veterinarian>,
+  ): Promise<Partial<PgVeterinarianInsertModel>> {
+    return {
+      id: entity.id,
+      fullname: entity.fullname,
+      email: entity.email?.get(),
+      phone: entity.phone?.get(),
+      license: entity.licenseNumber?.get(),
+      createdAt: entity.createdAt,
+      updatedAt: entity.updatedAt,
+    };
+  }
+
+  async toEntity(model: PgVeterinarianModel): Promise<Veterinarian> {
+    const email = Email.create({
+      email: model.email,
+      emailValidator: this.deps.emailValidator,
+    });
+    if (!email.ok) throw email.error;
+
+    const phone = Phone.create({
+      phone: model.phone,
+      phoneValidator: this.deps.phoneValidator,
+    });
+    if (!phone.ok) throw phone.error;
+
+    const licenseNumber = License.create({
+      license: model.license,
+      licenseValidator: this.deps.licenseValidator,
+    });
+    if (!licenseNumber.ok) throw licenseNumber.error;
+
+    const veterinarian = Veterinarian.create({
+      id: model.id,
+      fullname: model.fullname,
+      email: email.value,
+      phone: phone.value,
+      licenseNumber: licenseNumber.value,
+      createdAt: model.createdAt,
+      updatedAt: model.updatedAt,
+    });
+
+    if (!veterinarian.ok) throw veterinarian.error;
+
+    return veterinarian.value;
+  }
+}
