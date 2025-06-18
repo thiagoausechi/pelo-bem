043642a3ff319d39c1f4cb8e29a4e599a3e8a29d
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-17 21:53:51 -0300

    chore(global): 🔧 adicionar configurações para ignorar erros do ESLint e TypeScript durante a construção

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@ import { env } from "./backend/infrastructure/configs/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  eslint: {
+    /**
+     * Já rodamos o ESLint no CI, então podemos ignorar os erros durante a construção.
+     * Isso nos da um ganho de performance durante a build, pois não precisamos verificar o código novamente.
+     */
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    /**
+     * Já rodamos o checker do TypeScript no CI, então podemos ignorar os erros durante a construção.
+     * Isso nos da um ganho de performance durante a build, pois não precisamos verificar o código novamente.
+     */
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
