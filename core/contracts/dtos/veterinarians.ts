697894f3d644af97b874b1e47e63c931ba4c5356
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-19 15:21:57 -0300

    feat(global): ✨ adicionar interface `VeterinarianDTO`

---
+++ core/contracts/dtos/veterinarians.ts
@@ -0,0 +1,10 @@
+export interface VeterinarianDTO {
+  id: string;
+  profile?: string;
+  name: string;
+  email: string;
+  phone: string;
+  license: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
