bd94bb466481ab4887e2d966918a4069aa3d3d15
Lucas Facina <2411600015@fema.edu.br>
2025-06-20 13:06:28 -0300

    feat(server/infra): ✨ implementar `handleGet` em `NextJsVeterinariansController`

--- backend/infrastructure/adapters/http/next/controllers/veterinarians.ts
+++ backend/infrastructure/adapters/http/next/controllers/veterinarians.ts
@@ -1,9 +1,12 @@
+import type { VeterinarianDTO } from "@core/contracts/dtos/veterinarians";
 import { HttpStatus } from "@core/http";
 import type { VeterinarianGateway } from "@server/application/gateways";
 import type { FileStorageGateway } from "@server/application/gateways/file-storage";
+import { ListVeterinarianUseCase } from "@server/application/usecases/veterinarian";
 import type { EmailValidator } from "@server/domain/value-objects/email";
 import type { LicenseValidator } from "@server/domain/value-objects/license";
 import type { PhoneValidator } from "@server/domain/value-objects/phone";
+import { env } from "@server/infrastructure/configs/env";
 import type { NextRequest, NextResponse } from "next/server";
 import { NextJsController } from "./base";
 
@@ -20,8 +23,30 @@ export class NextJsVeterinariansController extends NextJsController {
     super();
   }
 
-  async handleGet(_: NextRequest): Promise<NextResponse> {
-    return HttpStatus.NOT_IMPLEMENTED();
+  async handleGet(request: NextRequest): Promise<NextResponse> {
+    return this.handleRequest(async () => {
+      const pathSegments = this.parsePath(request);
+      const id = pathSegments.length > 0 ? pathSegments[0] : undefined;
+
+      const useCaseResponse = await new ListVeterinarianUseCase(
+        this.deps,
+      ).execute({ filters: id! ? { id } : undefined });
+
+      const result: VeterinarianDTO[] = useCaseResponse.map(
+        ({ veterinarian }) => ({
+          id: veterinarian.id,
+          name: veterinarian.fullname,
+          email: veterinarian.email.get(),
+          phone: veterinarian.phone.get(),
+          license: veterinarian.license.get(),
+          profile: `${env.S3_PUBLIC_URL}/veterinarians/${veterinarian.id}.png`,
+          createdAt: veterinarian.createdAt,
+          updatedAt: veterinarian.updatedAt,
+        }),
+      );
+
+      return HttpStatus.OK(result);
+    });
   }
 
   async handlePost(_: NextRequest): Promise<NextResponse> {
