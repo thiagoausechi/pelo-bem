191cf13cda41721fcb89ff5e709d7c8ced5b60b7
Lucas Facina <2411600015@fema.edu.br>
2025-06-22 16:52:14 -0300

    refactor(client/pages): :lipstick: aumentar espaçamento entre as sessões do dashboard

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -8,7 +8,7 @@ export function DashboardPage() {
   return (
     <div className="relative isolate flex flex-1 flex-col">
       <div className="@container/content container mx-auto flex flex-1 flex-col gap-2 px-4">
-        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+        <div className="flex flex-col gap-4 py-4 md:gap-11 md:py-6">
           <StatsCardSection />
 
           <UpcomingServicesSection />
