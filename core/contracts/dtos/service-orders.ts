858b155609a9c9ac9515e89a2d488362c1b967bd
Lucas Facina <2411600015@fema.edu.br>
2025-06-22 16:47:51 -0300

    refactor(global): :label: atualizar `ServiceOrderDTO` para incluir detalhes da avaliação

--- core/contracts/dtos/service-orders.ts
+++ core/contracts/dtos/service-orders.ts
@@ -1,4 +1,4 @@
-import type { ServiceOrderStatus } from "../enums/service-orders";
+import type { Rating, ServiceOrderStatus } from "../enums/service-orders";
 import type { PetDTO } from "./pets";
 import type { VeterinarianDTO } from "./veterinarians";
 
@@ -11,13 +11,24 @@ export interface ServiceTypeDTO {
   updatedAt: Date;
 }
 
-export interface ServiceOrderDTO {
+export type ServiceOrderDTO = {
   id: string;
   pet: PetDTO;
   veterinarian: VeterinarianDTO;
   appointmentDate: Date;
   serviceType: ServiceTypeDTO;
-  status: ServiceOrderStatus;
   createdAt: Date;
   updatedAt: Date;
-}
+} & (
+  | {
+      status: "COMPLETED";
+      satisfaction: {
+        rating: Rating;
+        comment?: string;
+      };
+    }
+  | {
+      status: Omit<ServiceOrderStatus, "COMPLETED">;
+      satisfaction?: never;
+    }
+);
