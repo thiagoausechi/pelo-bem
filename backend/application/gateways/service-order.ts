f862f0d350a4f22d52b4213a7320e4841232c96a
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-22 14:42:00 -0300

    feat(server/app): ✨ adicionar `ServiceTypeGateway`

--- backend/application/gateways/service-order.ts
+++ backend/application/gateways/service-order.ts
@@ -1,4 +1,9 @@
-import type { ServiceOrder } from "@server/domain/entities/service-order";
+import type {
+  ServiceOrder,
+  ServiceType,
+} from "@server/domain/entities/service-order";
 import type { BaseGateway } from "./base/gateway";
 
 export type ServiceOrderGateway = BaseGateway<ServiceOrder>;
+
+export type ServiceTypeGateway = BaseGateway<ServiceType>;
