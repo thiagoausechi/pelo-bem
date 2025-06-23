734deb3b444915d16e4f272bf5cddfcd1e2ea56d
Lucas Facina <2411600015@fema.edu.br>
2025-06-23 00:32:34 -0300

    refactor(server/app): :recycle: alterar retorno do `ListServiceOrdersUserCase`

--- backend/application/usecases/service-order/list.ts
+++ backend/application/usecases/service-order/list.ts
@@ -1,13 +1,30 @@
-import type { ServiceOrderGateway } from "@server/application/gateways";
+import type {
+  OwnerGateway,
+  PetGateway,
+  ServiceOrderGateway,
+  ServiceTypeGateway,
+  VeterinarianGateway,
+} from "@server/application/gateways";
 import type {
   Entry,
   FiltersFor,
   ListOptions,
 } from "@server/application/gateways/base/gateway";
-import type { ServiceOrder } from "@server/domain/entities/service-order";
+import type { Owner } from "@server/domain/entities/owner";
+import type { Pet } from "@server/domain/entities/pet";
+import type {
+  Satisfaction,
+  ServiceOrder,
+  ServiceType,
+} from "@server/domain/entities/service-order";
+import type { Veterinarian } from "@server/domain/entities/veterinarian";
 
 interface Dependencies {
   serviceOrderGateway: ServiceOrderGateway;
+  serviceTypeGateway: ServiceTypeGateway;
+  veterinarianGateway: VeterinarianGateway;
+  petGateway: PetGateway;
+  ownerGateway: OwnerGateway;
 }
 
 interface Request {
@@ -17,19 +34,82 @@ interface Request {
 
 interface Response {
   serviceOrder: Entry<ServiceOrder>;
+  serviceType: Entry<ServiceType>;
+  satisfaction?: Entry<Satisfaction>;
+  veterinarian: Entry<Veterinarian>;
+  pet: Entry<Pet>;
+  owner: Entry<Owner>;
 }
 
 export class ListServiceOrdersUserCase {
+  private serviceOrders: Entry<ServiceOrder>[] = [];
+
   constructor(private deps: Dependencies) {}
 
   async execute(request: Request): Promise<Response[]> {
-    const serviceOrders = await this.deps.serviceOrderGateway.listAll(
+    this.serviceOrders = await this.deps.serviceOrderGateway.listAll(
       request.filters,
       request.options,
     );
 
-    return serviceOrders.map((serviceOrder) => ({
+    const [serviceTypes, veterinarians, pets] = await Promise.all([
+      Promise.all(this.getServiceTypes()),
+      Promise.all(this.getVeterinarians()),
+      Promise.all(this.getPets()),
+    ]);
+
+    const serviceTypesValues = serviceTypes.map((serviceType) => {
+      if (!serviceType.ok) throw serviceType.error;
+      return serviceType.value;
+    });
+
+    const veterinariansValues = veterinarians.map((veterinarian) => {
+      if (!veterinarian.ok) throw veterinarian.error;
+      return veterinarian.value;
+    });
+
+    const petsValues = pets.map((pet) => {
+      if (!pet.ok) throw pet.error;
+      return pet.value;
+    });
+
+    const owners = await Promise.all(
+      petsValues.map(({ ownerId }) =>
+        this.deps.ownerGateway.findBy({ id: ownerId }),
+      ),
+    );
+
+    const ownersValues = owners.map((owner) => {
+      if (!owner.ok) throw owner.error;
+      return owner.value;
+    });
+
+    return this.serviceOrders.map((serviceOrder, index) => ({
       serviceOrder,
+      serviceType: serviceTypesValues[index]!,
+      veterinarian: veterinariansValues[index]!,
+      pet: petsValues[index]!,
+      owner: ownersValues[index]!,
+      // TODO: Retornar aqui ao implementar a funcionalidade de satisfação
+      satisfaction: undefined,
     }));
+  }
+
+  private getServiceTypes() {
+    return this.serviceOrders.map(({ serviceTypeId }) =>
+      this.deps.serviceTypeGateway.findBy({ id: serviceTypeId }),
+    );
+  }
+
+  private getVeterinarians() {
+    return this.serviceOrders.map(({ veterinarianId }) =>
+      this.deps.veterinarianGateway.findBy({ id: veterinarianId }),
+    );
+  }
+
+  private getPets() {
+    return this.serviceOrders.map(({ petId }) =>
+      this.deps.petGateway.findBy({ id: petId }),
+    );
   }
 }
