f1ed9a42ac02ece26b002fe19f6e45c4a33ee06b
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-23 09:27:41 -0300

    feat(server/app): ✨ implementar `ListServiceTypesUseCase`

---
+++ backend/application/usecases/service-order/type/list.ts
@@ -0,0 +1,34 @@
+import type { ServiceTypeGateway } from "@server/application/gateways";
+import type {
+  FiltersFor,
+  ListOptions,
+} from "@server/application/gateways/base/gateway";
+import type { ServiceType } from "@server/domain/entities/service-order";
+
+interface Dependencies {
+  serviceTypeGateway: ServiceTypeGateway;
+}
+
+interface Request {
+  filters?: FiltersFor<ServiceType>;
+  options?: ListOptions<keyof ServiceType>;
+}
+
+interface Response {
+  serviceType: ServiceType;
+}
+
+export class ListServiceTypesUseCase {
+  constructor(private deps: Dependencies) {}
+
+  async execute(request: Request): Promise<Response[]> {
+    const serviceTypes = await this.deps.serviceTypeGateway.listAll(
+      request.filters,
+      request.options,
+    );
+
+    return serviceTypes.map((serviceType) => ({
+      serviceType,
+    }));
+  }
+}
