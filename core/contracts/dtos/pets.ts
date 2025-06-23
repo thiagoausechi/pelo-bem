ef74b547edace895558080ff7fe690b2b16e7292
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-23 10:17:02 -0300

    refactor(global): ♻️ refatorar `PetDTO` para utilizar validação do `Zod`

--- core/contracts/dtos/pets.ts
+++ core/contracts/dtos/pets.ts
@@ -1,18 +1,21 @@
-import type { Gender, Specie } from "../enums/pets";
-import type { OwnerDTO } from "./owners";
+import { z } from "zod";
+import { genders, species } from "../enums/pets";
+import { ownerDtoSchema } from "./owners";
 
-export interface PetDTO {
-  id: string;
-  owner: OwnerDTO;
-  picture?: string;
-  name: string;
-  specie: Specie;
-  breed: string;
-  age: string; // Para visualização, ex: "2 anos"
-  birthday: Date; // Para ordenação
-  weightKg: number;
-  heightCm: number;
-  gender: Gender;
-  createdAt: Date;
-  updatedAt: Date;
-}
+export const petDtoSchema = z.object({
+  id: z.string(),
+  owner: ownerDtoSchema,
+  picture: z.string().optional(),
+  name: z.string(),
+  specie: z.enum(species),
+  breed: z.string(),
+  age: z.string(), // Para visualização, ex: "2 anos"
+  birthday: z.coerce.date(), // Para ordenação
+  weightKg: z.number(),
+  heightCm: z.number(),
+  gender: z.enum(genders),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+});
+
+export type PetDTO = z.infer<typeof petDtoSchema>;
