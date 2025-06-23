f6415b50a5162cc6cdfd2c05edef66182e177058
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-06-23 10:14:59 -0300

    refactor(global): ♻️ refatorar `OwnerDTO` para utilizar validação do `Zod`

--- core/contracts/dtos/owners.ts
+++ core/contracts/dtos/owners.ts
@@ -1,9 +1,13 @@
-export interface OwnerDTO {
-  id: string;
-  profile?: string;
-  name: string;
-  email: string;
-  phone: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
+import { z } from "zod";
+
+export const ownerDtoSchema = z.object({
+  id: z.string(),
+  profile: z.string().optional(),
+  name: z.string(),
+  email: z.string().email(),
+  phone: z.string(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+});
+
+export type OwnerDTO = z.infer<typeof ownerDtoSchema>;
